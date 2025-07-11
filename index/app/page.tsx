e442b6cd3944936a7abe35b1f458b4b45a1cb1d1
Simon Swart <simonswart91@gmail.com>
2025-07-11 23:10:30 +0200

    Update page.tsx

--- index/app/page.tsx
+++ index/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const MyopiaSimulator = () => {
   const sliderNumbersRef = useRef<HTMLDivElement | null>(null);
