9db47a69e6f4ca7e5388991e557c664b040dc8f5
Simon Swart <simonswart91@gmail.com>
2025-08-15 14:57:19 +0200

    :(

--- index/next.config.ts
+++ index/next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    experimental: {
-    optimizeCss: false, // disable Next.js CSS optimization
-  },
-  swcMinify: false, // disable JS/CSS minification
-};
 
 export default nextConfig;
